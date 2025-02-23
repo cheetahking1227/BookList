d88df9ae34dc2145f2980b9216012d0f1216b0d0
cheetahking1227 <cheetahking1227@gmail.com>
2025-02-23 13:16:29 -0600

    feat: build home screen

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,26 +6,24 @@ import { BookItem } from '../../components/BookItem';
 import { getBooks } from '../../actions';
 import { BookType } from '../../types';
 
-const Home = () => {
-    const [books, setBooks] = useState<BookType[] | undefined>([]);
+export const Home = () => {
+	const [books, setBooks] = useState<BookType[] | undefined>([]);
 
-    useEffect(() => {
-        const fetchBooks = async () => {
-            const data = await getBooks();
-            setBooks(data);
-        }
-        fetchBooks();
-    }, []);
+	useEffect(() => {
+		const fetchBooks = async () => {
+			const data = await getBooks();
+			setBooks(data);
+		}
+		fetchBooks();
+	}, []);
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <FlatList
-                data={books}
-                renderItem={BookItem}
-                keyExtractor={(_, index) => index.toString()}
-            />
-        </SafeAreaView>
-    )
+	return (
+		<SafeAreaView style={styles.container}>
+			<FlatList
+				data={books}
+				renderItem={BookItem}
+				keyExtractor={(_, index) => index.toString()}
+			/>
+		</SafeAreaView>
+	)
 }
-
-export default Home;
